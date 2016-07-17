6cdd515574a3472eacd85c49255d7097ba33867a
William Wright <william86378@williams-mac.local>
2016-07-17 14:07:33 -0700

    Initial Commit

---
+++ Pokémon Go Server Status/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokémon Go Server Status
+//
+//  Created by william wright  on 7/17/16.
+//  Copyright © 2016 A.R.C software and enginering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
